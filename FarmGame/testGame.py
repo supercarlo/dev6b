728f40136f2381800bc65fd24fa6573cd70993ad
Vitrav <dave.visser@outlook.com>
2016-12-24 16:34:17 +0100

    playerStatus in testgame

--- FarmGame/testGame.py
+++ FarmGame/testGame.py
@@ -2,10 +2,8 @@ from FarmGame import FarmStock
 from FarmGame import Player
 
 def game():
+    playerStatus()
     x = str(input("buy, sell or exit      "))
-    print("Player money:   " + str(Player.Player.money))
-    print("CORN amount in bag:  " + str(Player.Player.corn))
-    print("WHEAT amount in bag:  " + str(Player.Player.wheat))
 
 
     if x == "buy":
@@ -17,10 +15,6 @@ def game():
             CornAmount = str(input("How many would you like to buy?   "))
             FarmStock.Corn.buyCornAmount(int(CornAmount))
 
-        print("Player money:   " + str(Player.Player.money))
-        print("CORN amount in bag:  " + str(Player.Player.corn))
-        print("WHEAT amount in bag:  " + str(Player.Player.wheat))
-
     if x == "sell":
         y = str(input("What would you like to sell? Wheat (10 each) or corn (20 each)"))
         if y == "Wheat" or y == "wheat":
@@ -30,10 +24,17 @@ def game():
             amount = str(input("How many would you like to sell?   "))
             FarmStock.Corn.sellCornAmount(int(amount))
 
+
     if x != "exit":
         game()
     else:
         exit()
+
+
+def playerStatus():
+    print("Player money:   " + str(Player.Player.money))
+    print("CORN amount in bag:  " + str(Player.Player.corn))
+    print("WHEAT amount in bag:  " + str(Player.Player.wheat))
 
 
 game()
